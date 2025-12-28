167347892f1a93714647de5e45534cdc3a098e78
islam dev <islamdebbi6gmial.com>
2025-12-28 21:21:48 +0100

    add clip path

--- lib/animations/animation_screen.dart
+++ lib/animations/animation_screen.dart
@@ -29,7 +29,6 @@ class _AnimationScreenState extends State<AnimationScreen>
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     _controller.dispose();
   }
@@ -37,33 +36,98 @@ class _AnimationScreenState extends State<AnimationScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return Transform(
-              alignment: Alignment.center,
-              // origin: const Offset(50, 50),
-              transform: Matrix4.identity()..rotateY(_animation.value),
-              child: Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      blurRadius: 10,
-                      offset: const Offset(0, 5),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(width: 100, height: 100, color: Colors.red),
+                Container(width: 100, height: 100, color: Colors.black87),
+              ],
+            ),
+            SizedBox(height: 50),
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return GestureDetector(
+                  onTap: () {
+                    if (_controller.isAnimating) {
+                      _controller.stop();
+                    } else {
+                      _controller.repeat();
+                    }
+                  },
+                  child: Transform(
+                    alignment: Alignment.center,
+                    // origin: const Offset(50, 50),
+                    transform: Matrix4.identity()..rotateZ(_animation.value),
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            blurRadius: 10,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-              ),
-            );
-          },
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class HalfCircleClipper extends CustomClipper<Path> {
+  final CircleSide side;
+
+  const HalfCircleClipper({required this.side});
+
+  @override
+  Path getClip(Size size) {
+    return side.toPath(size);
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+enum CircleSide { left, right }
+
+extension ToPath on CircleSide {
+  Path toPath(Size size) {
+    final path = Path();
+    late Offset offset;
+    late bool clockwise;
+    switch (this) {
+      case CircleSide.left:
+        path.moveTo(size.width, 0);
+        offset = Offset(size.width, size.height);
+        clockwise = false;
+        break;
+      case CircleSide.right:
+        offset = Offset(0, size.height);
+        clockwise = true;
+        break;
+    }
+    path.arcToPoint(
+      offset,
+      radius: Radius.elliptical(size.width / 2, size.height / 2),
+      clockwise: clockwise,
+    );
+    path.close();
+    return path;
   }
 }
