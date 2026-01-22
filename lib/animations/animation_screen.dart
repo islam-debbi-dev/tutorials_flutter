0b38d9d4a94f8f8e2b6ad9a3c203db445e5c72a3
islam dev <islamdebbi6gmial.com>
2026-01-22 23:01:25 +0100

    add MyDrawer widget for enhanced animation control in AnimationScreen

--- lib/animations/animation_screen.dart
+++ lib/animations/animation_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tutorials_flutter/animations/widgets/first_animation_widget.dart';
 
 import 'widgets/change_colors_animation.dart';
+import 'widgets/drawer/my_drawer.dart';
 
 class AnimationScreen extends StatefulWidget {
   const AnimationScreen({super.key});
@@ -13,19 +14,33 @@ class AnimationScreen extends StatefulWidget {
 class _AnimationScreenState extends State<AnimationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            const FirstAnimationWidget(),
-            SizedBox(height: 50),
-            // const SecondAnimationWidget(),
-            const SizedBox(height: 50),
+    return MyDrawer(
+      drawer: Material(
+        child: Container(
+          color: const Color.fromARGB(255, 17, 32, 45),
+          child: ListView.builder(
+            itemCount: 7,
+            itemBuilder: (context, index) {
+              return ListTile(title: Text('Item ${index + 1}'));
+            },
+          ),
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Animations Screen')),
+        body: SafeArea(
+          child: Column(
+            children: [
+              // const FirstAnimationWidget(),
+              // SizedBox(height: 50),
+              // // const SecondAnimationWidget(),
+              // const SizedBox(height: 50),
 
-            // const ImplicitAnimationWidget(),
-            // const SizedBox(height: 50),
-            const ChangeColorsAnimation(),
-          ],
+              // // const ImplicitAnimationWidget(),
+              // // const SizedBox(height: 50),
+              // const ChangeColorsAnimation(),
+            ],
+          ),
         ),
       ),
     );
